a985174ced8535b3e68ba776104210eb097ef572
Christan Söderberg <christian.v.soderberg@gmail.com>
2023-10-27 11:22:33 +0200

    Hide/show kiwi with CSS Fixes #21

--- src/scripts/checkKiwi.js
+++
@@ -1,7 +0,0 @@
-function checkKiwi() {
-  // Om fönstret är för litet så visas inte kiwifågel
-  if (window.innerWidth < 1280)
-    // längst ner i höger hörn
-    document.getElementById("kiwi").style.visibility = "hidden";
-  else document.getElementById("kiwi").style.visibility = "visible";
-}
